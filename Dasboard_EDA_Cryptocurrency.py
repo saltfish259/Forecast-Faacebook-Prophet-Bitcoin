6bc7c88a51e553625958ff0047603aad32984b03
Razif Zulvikar H <96932032+saltfish259@users.noreply.github.com>
2025-04-27 13:20:09 +0700

    Update Dasboard_EDA_Cryptocurrency.py

--- Dasboard_EDA_Cryptocurrency.py
+++ Dasboard_EDA_Cryptocurrency.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNm7WGY7pBJevwiIjj/btgK",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
